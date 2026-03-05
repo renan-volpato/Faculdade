d02ca0f99506396b401df3b754e9e346833ac7af
Renan Volpato <renanvolpato63@gmail.com>
2026-03-05 02:37:07 -0300

    Implement getters, setters, and constructors in Exame
    Added getters, setters, and constructors to Exame class.

---
+++ poo/atividade 3/exame.java
@@ -0,0 +1,58 @@
+/* Utilizando a correção da atividade 2 modifique as classes colocando os gets 
+e sets e criando os 2 construtores um sem parametros e outro para 
+inicialização de cada atributo e o método mostrar de cada classe */
+
+import java.time.LocalDate;
+
+public class Exame {
+  public Date data;
+  public String consulta;
+  public String descritivo;
+
+  public Exame() {
+  }
+
+  public Exame(LocalDate data, String consulta, String descritivo) {
+    this.data = data;
+    this.consulta = consulta;
+    this.descritivo = descritivo;
+  }
+
+  public LocalDate getData() {
+    return data;
+  }
+
+  public void setData(LocalDate data) {
+    this.data = data;
+  }
+
+  public String getConsulta() {
+    return consulta;
+  }
+
+  public void setConsulta(String consulta) {
+    this.consulta = consulta;
+  }
+
+  public String getDescritivo() {
+    return descritivo;
+  }
+
+  public void setDescritivo(String descritivo) {
+    this.descritivo = descritivo;
+  }
+
+  public void solicitar () {
+    System.out.println("solicitado");
+  }
+
+  public void consultar () {
+    System.out.println("consultado");
+  }
+  
+  public void mostrar () {
+    System.out.println("data: " + data);
+    System.out.println("consulta: " + consulta);
+    System.out.println("descrição: " + descritivo);
+  }  
+}
