6b0f0f23c6e00ed123be2fc354c338991fe2efc2
Renan Volpato <renanvolpato63@gmail.com>
2026-04-01 13:27:36 -0300

    Create Exame class for medical exam management
    This class represents a medical exam with attributes for date, description, consultation, and doctor. It includes methods for setting and getting these attributes, along with validation for the description.

---
+++ poo/atividade 5/exame.java
@@ -0,0 +1,50 @@
+package com.fatec.ads;
+
+public class Exame {
+    private String data;
+    private String descritivo;
+    private Consulta consulta;
+    private Medico medico;
+
+    public void solicitar(){}
+    public void consultar(){}
+
+    public Exame(String data, String descritivo,
+        Consulta consulta, Medico medico) {
+        this.consulta = consulta;
+        this.data = data;
+        this.descritivo = descritivo;
+        this.medico = medico;
+    }
+
+    public String getConsulta() {
+        return consulta;
+    }
+    public void setConsulta(String consulta) {
+        this.consulta = consulta;
+    }
+    public String getData() {
+        return data;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+    public String getDescritivo() {
+        return descritivo;
+    }
+    public void setDescritivo(String descritivo) throws Exception {
+        if(descritivo==null || descritivo.length()<=0)
+            throw new Exception("Informe o descritvo do exame");
+        this.descritivo = descritivo;
+    }
+
+    public Exame() {
+    }
+   
+    public void mostrar() {
+        var s = "Exame [getConsulta()=" + getConsulta() + ", getData()=" + getData() + ", getDescritivo()="
+                + getDescritivo() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+                + super.toString() + "]";
+        System.out.println(s);
+    }   
+}
