0ab8873f535f46b41e4098233507b0fd80af7fd5
Renan Volpato <renanvolpato63@gmail.com>
2026-04-01 13:24:16 -0300

    Create Agenda class with consultation management

---
+++ poo/atividade 5/agenda.java
@@ -0,0 +1,79 @@
+/* Usando a correção da atividade anterior, crie as associações, composições e 
+agregaçoes propostas no diagrama de classe abaixo: */
+
+package com.fatec.ads;
+
+import java.util.ArrayList;
+
+public class Agenda {
+    String data;
+    String hora;
+    String medico;
+    String paciente;
+
+    public void consultar(){}
+
+    private List<Consulta> consultas;
+
+    public Agenda() {
+        this.consultas = new ArrayList<>();
+    }
+
+    public void adicionarConsulta(Consulta c) {
+        consultas.add(c);
+    }
+
+    public List<Consulta> getConsulta() {
+        return consultas;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) throws  ExceptionClinica{
+        if(data==null){
+            throw new ExceptionClinica("AS2345234", "A data nao pode ser nula!");
+        } else {
+            this.data = data;
+        }
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getMedico() {
+        return medico;
+    }
+
+    public void setMedico(String medico) {
+        this.medico = medico;
+    }
+
+    public String getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(String paciente) {
+        this.paciente = paciente;
+    }
+
+    public Agenda(String data, String hora, String medico, String paciente) {
+        this.data = data;
+        this.hora = hora;
+        this.medico = medico;
+        this.paciente = paciente;
+    }
+  
+    public void mostrar() {
+        var s = "Agenda [getData()=" + getData() + ", getHora()=" + getHora() + ", getMedico()=" + getMedico()
+                + ", getPaciente()=" + getPaciente() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+                + ", toString()=" + super.toString() + "]";
+        System.out.println(s);
+    }
+}
