e1aaa003bea3fd5a68b3ffd6183a3001638fc240
Renan Volpato <renanvolpato63@gmail.com>
2026-05-15 23:13:50 -0300

    Create Agenda class with consultation details

---
+++ poo/atividade 9/agenda.java
@@ -0,0 +1,15 @@
+public class Agenda {
+    public String data;
+    public String hora;
+    public String medico;
+    public String paciente;
+
+    public void consultar(){
+    }
+    public void mostrar(){
+        System.out.println("data:"+data);
+        System.out.println("hora:"+hora);
+        System.out.println("medico:"+medico);
+        System.out.println("paciente:"+paciente);
+    }
+}
